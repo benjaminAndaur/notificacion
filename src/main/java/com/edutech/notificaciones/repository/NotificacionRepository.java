079fee7168c79d893877a40aa44cd6a94f89604c
BenjaAndaur <benjamin.andaur.m@gmail.com>
2025-05-22 13:00:40 -0400

    Mejoras Proyecto

--- src/main/java/com/edutech/notificaciones/repository/NotificacionRepository.java
+++ src/main/java/com/edutech/notificaciones/repository/NotificacionRepository.java
@@ -1,7 +1,5 @@
 package com.edutech.notificaciones.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.edutech.notificaciones.model.Notificacion;
